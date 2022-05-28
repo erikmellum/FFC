0b1f9884db4d8032e915cd714041864283abbf2e
developer1 <developer1@sitehatchery.com>
2022-05-28 06:43:12 +0530

    #FFCMM-6,7,8 - Resolved issue of marker window opening only after double click. - Set Marker color red - Hide email link in marker window

--- src/components/mapMarker/marker.tsx
+++ src/components/mapMarker/marker.tsx
@@ -8,25 +8,29 @@ import {sendOutline, closeOutline} from 'ionicons/icons';
 
 const Marker = (props: any) => {
   const [showHint, setShowHint] = useState<Boolean>(false);
-  const handleClick = () => {
+  const handleClick = (event: React.MouseEvent, missionary_id: any) => {
     var i;
     var allElements = document.querySelectorAll(".hint");
     for (i = 0; i < allElements.length; i++) {
-      allElements[i].classList.remove('active');
+      if (allElements[i].classList.contains("hint_" + missionary_id)) {
+        allElements[i].classList.toggle("active");
+      } else {
+        allElements[i].classList.remove('active');
+      }
     }
-    setShowHint(!showHint);
+    //setShowHint(!showHint);
   }
   return <>
-    <div className="pin" onClick={handleClick}></div>
+    <div className="pin" onClick={(event) => handleClick(event, props.missionary.id)}></div>
     <div className="pulse"></div>
-    <div className={`text ${props.missionary.position}`} onClick={handleClick}>{props.missionary.title}</div>
-      <div className={`hint ${props.missionary.position} ${showHint ? "active" : ""}`}>
-        <IonIcon className="map-marker-close-btn ion-float-right " icon={closeOutline} onClick={handleClick}/>
+    <div className={`text ${props.missionary.position}`} onClick={(event) => handleClick(event, props.missionary.id)}>{props.missionary.title}</div>
+      <div className={`hint hint_${props.missionary.id} ${props.missionary.position}`}>
+        <IonIcon className="map-marker-close-btn ion-float-right " icon={closeOutline} onClick={(event) => handleClick(event, props.missionary.id)}/>
         <div className="hint-image-container">
           <img src={props.missionary.photo}/>
         </div>
         <span>{props.missionary.description}</span>
-        <a className="send-cta" href={`mailto:${props.missionary.email}?subject=Hello from Firm Family Church!&body=We are thinking of you!`} target="ffc">
+        <a className="send-cta ion-hide" href={`mailto:${props.missionary.email}?subject=Hello from Firm Family Church!&body=We are thinking of you!`} target="ffc">
           <span>Send a message</span> 
           <IonIcon icon={sendOutline} />
         </a>
