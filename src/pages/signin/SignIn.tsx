363877892b624de3e327da947c76aef4fe403525
developer1 <developer1@sitehatchery.com>
2022-06-24 18:08:15 +0530

    #FFCMM-13 - Wrong credentials message on Signin page. - Show and hide password on Signin page.

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,47 +1,59 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonInput, IonButton } from '@ionic/react';
-import React, { useState } from 'react';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonInput, IonButton, IonLabel, IonIcon, IonText} from '@ionic/react';
+import React, {useState} from 'react';
 import './SignIn.scss';
+import {eye, eyeOff} from "ionicons/icons";
 
 interface SignInProps {
-  setSignedIn: any
+    setSignedIn: any
 }
 
-const Home: React.FC<SignInProps> = ({ setSignedIn}) => {
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
-  const submit = (username?: string, password?: string) => {
-    if (username === 'ffc' && password === 'missionarieshavethemostfun') {
-      setSignedIn(true);
-    } else {
-      setSignedIn(false);
-    }
-  }
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>FFC Sign In</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">FFC Sign In</IonTitle>
-          </IonToolbar>
+const Home: React.FC<SignInProps> = ({setSignedIn}) => {
+    const [username, setUsername] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [authenticated, setAuthenticated] = useState<boolean>();
+    const [passwordShown, setPasswordShown] = useState(false);
+    const submit = (username?: string, password?: string) => {
+        if (username === 'ffc' && password === 'missionarieshavethemostfun') {
+            setSignedIn(true);
+            setAuthenticated(true);
+        } else {
+            setSignedIn(false);
+            setAuthenticated(false);
+        }
+    };
+    const togglePasswordVisibility = () => {
+        setPasswordShown(!passwordShown);
+    };
+    return <IonPage>
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>FFC Sign In</IonTitle>
+            </IonToolbar>
         </IonHeader>
-        <div className="container">
-          <img src="assets/ffc-logo.png" alt="FFC"/>
-          <IonItem>
-            <IonInput value={username} placeholder="Enter username" onIonChange={e => setUsername(e.detail.value!)} onKeyPress={(e) => e.key === 'Enter' && submit(username, password)}></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonInput value={password} type="password" placeholder="Enter password" onIonChange={e => setPassword(e.detail.value!)} onKeyPress={(e) => e.key === 'Enter' && submit(username, password)}></IonInput>
-          </IonItem>
-          <IonButton expand="full" onClick={e => submit(username, password)}>Submit</IonButton>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
+        <IonContent fullscreen>
+            <IonHeader collapse="condense">
+                <IonToolbar>
+                    <IonTitle size="large">FFC Sign In</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <div className="container">
+                <img src="assets/ffc-logo.png" alt="FFC"/>
+                <IonItem color="danger" className={authenticated === false ? 'ion-show' : 'ion-hide'} >
+                    <IonLabel className="ion-text-preline">Your username or password was incorrect.</IonLabel>
+                </IonItem>
+                <IonItem>
+                    <IonInput value={username} placeholder="Enter username" onIonChange={e => setUsername(e.detail.value!)} onKeyPress={(e) => e.key === 'Enter' && submit(username, password)}></IonInput>
+                </IonItem>
+                <IonItem>
+                    <IonInput value={password} type={passwordShown ? "text" : "password"} placeholder="Enter password" onIonChange={e => setPassword(e.detail.value!)} onKeyPress={(e) => e.key === 'Enter' && submit(username, password)}></IonInput>
+                    <span className="password-icon" title={passwordShown ? 'Hide Password' : 'Show Password'} onClick={togglePasswordVisibility}>
+                      <IonIcon icon={passwordShown ? eyeOff : eye}></IonIcon>
+                    </span>
+                </IonItem>
+                <IonButton expand="full" onClick={e => submit(username, password)}>Submit</IonButton>
+            </div>
+        </IonContent>
+    </IonPage>;
 };
 
 export default Home;
